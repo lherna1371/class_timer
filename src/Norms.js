4dea61a74cebbdd6e9330094fe8e77105e657c52
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2021-08-04 18:55:15 -0500

    add missing period

--- src/Norms.js
+++ src/Norms.js
@@ -12,7 +12,7 @@ export default class Norms extends React.Component {
           <ul>
             <li>Follow NJIT’s code of ethics</li>
             <li>Mute yourself</li>
-            <li>Put pronouns as part of your Zoom names</li>
+            <li>Put pronouns as part of your Zoom names.</li>
             <li>Raise hand or type in chat for questions</li>
             <li>We strongly encourage turning on video, so you feel present in the classroom</li>
             <li>Be respectful</li>
